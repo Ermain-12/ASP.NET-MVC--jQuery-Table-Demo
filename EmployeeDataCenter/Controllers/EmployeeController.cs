41e57a318947f04959033f3acb0adf0330c70736
Ermain <paulermain@gmail.com>
2018-06-22 00:07:20 -0400

    Implemented functionality to display SQL data to the user table. TODO: Add server-side functionality

---
+++ EmployeeDataCenter/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeDataCenter.Models;
+
+namespace EmployeeDataCenter.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetList()
+        {
+            using(EmployeeTableModel db = new EmployeeTableModel())
+            {
+                var empList = db.Employees.ToList();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
